93d2423f3682aadb4f8f623c8a0900381f3b8284
Rafał Walasek <rafal_walasek@interia.pl>
2018-12-15 12:40:31 +0100

    select MyShop

--- src/main/java/projectJDBC/MyShop.java
+++ src/main/java/projectJDBC/MyShop.java
@@ -4,7 +4,7 @@ import com.mysql.jdbc.Driver;
 
 import java.sql.*;
 
-public class ShopApp {
+public class MyShop {
     private static final String DB_URL = "jdbc:mysql://localhost/produkty";
     private static final String USER = "newuser";
     private static final String PASS = "brak";
@@ -30,14 +30,7 @@ public class ShopApp {
         //query execution
         Statement statement = null;
         PreparedStatement preparedStatement = null;
-
         try {
-//            preparedStatement = connection.prepareStatement(
-//                        "UPDATE products "+
-//                            "SET description = ? WHERE product_id = ?"
-//            );
-            //updateProducts(preparedStatement);
-
             statement = connection.createStatement();
             switch (1){
                 case 1:{
@@ -49,9 +42,6 @@ public class ShopApp {
                     break;
                 }
             }
-            //insertProduct(statement);
-            //finalAllQuery(statement);
-
         } catch (SQLException e) {
             e.printStackTrace();
         } finally{
@@ -79,13 +69,12 @@ public class ShopApp {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
-//-------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------
     private static void addOneProduct(Statement statement) {
         try {
             int inserted = statement.executeUpdate(
-                        "INSERT INTO products VALUES "+
+                    "INSERT INTO products VALUES "+
                             "(1,100,'komputer',null)"
             );
             System.out.println(inserted+" new products added.");
@@ -125,62 +114,4 @@ public class ShopApp {
             }
         }
     }
-
-    //-------------------------------------------------------------------------------------------------------------
-
-//    private static void updateProducts(PreparedStatement preparedStatement) {
-//        try {
-//            preparedStatement.setString(1,"bla bla bla");
-//            preparedStatement.setInt(2,1);
-//
-//            int update = preparedStatement.executeUpdate();
-//
-//            System.out.println(update+" updated products.");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    private static void finalAllQuery(Statement statement) {
-//        ResultSet resultSet = null;
-//        try {
-//            resultSet = statement.
-//                    executeQuery("SELECT product_id,name,description FROM products");
-//
-//            while(resultSet.next()){
-//                int product_id = resultSet.getInt("product_id");
-//                String name = resultSet.getString("name");
-//                String description = resultSet.getString("description");
-//
-//                System.out.println(
-//                                "Product with id: "+product_id+
-//                                " and name: "+name+
-//                                " and description: "+description+"."
-//                );
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if(resultSet != null){
-//                try {
-//                    resultSet.close();
-//                }catch (SQLException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//    }
-//    private static void insertProduct(Statement statement) {
-//        try {
-//            int inserted = statement.executeUpdate(
-//                        "INSERT INTO products VALUES "+
-//                            "(1,123456,'first product',null)"
-//            );
-//
-//            System.out.println(inserted+" new products added.");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 }
